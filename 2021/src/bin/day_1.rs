cd908195e00086797a8a3275a2c51b62cd8f828a
Samuel Collins <samuel.collins@live.co.uk>
2021-12-01 09:23:21 +0000

    2021 - day 1

--- 2021/src/bin/day_1.rs
+++ 2021/src/bin/day_1.rs
@@ -1,3 +1,23 @@
+#![feature(array_windows)]
+
 fn main() {
-    println!("Day 1 stub");
+    let depths: Vec<i32> = std::fs::read_to_string("input/day1.txt")
+        .unwrap()
+        .split('\n')
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let part1 = depths.array_windows().filter(|[x, y]| x < y).count();
+    println!("Part 1: {}", part1);
+
+    let part2 = depths
+        .array_windows()
+        .map(|[x, y, z]| x + y + z)
+        .collect::<Vec<i32>>()
+        .array_windows()
+        .filter(|[x, y]| x < y)
+        .count();
+
+    println!("Part 2: {}", part2);
 }
