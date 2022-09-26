858ced088b32b532595c54bb0e0a4626acfdc7b2
Samuel Collins <samuel.collins@live.co.uk>
2022-09-26 22:15:24 +0100

    2019: day 7 - part 2

--- 2019/src/bin/day_7.rs
+++ 2019/src/bin/day_7.rs
@@ -4,6 +4,7 @@ use util::PerfTimer;
 
 extern crate util;
 
+#[derive(Clone)]
 struct State {
     mem: Vec<isize>,
     pos: usize,
@@ -123,6 +124,52 @@ fn run_program(mem: Vec<isize>, mut input: Vec<isize>) -> isize {
     }
 }
 
+fn run_one_in_one_out(state: &mut State, input: isize) -> Option<isize> {
+    let mut input = Some(input);
+    let mut output = None;
+    let mut last_result = Code::Success(state.clone());
+
+    loop {
+        match last_result {
+            Code::Success(s) => {
+                if output.is_some() {
+                    *state = s;
+                    break output;
+                }
+                last_result =
+                    execute_operation(s, &mut || input.take().unwrap(), &mut |x| output = Some(x))
+            }
+            Code::Stop(s) => {
+                *state = s;
+                break output;
+            }
+        }
+    }
+}
+
+fn run_one_in(state: &mut State, input: isize) -> bool {
+    let mut input = Some(input);
+    let mut last_result = Code::Success(state.clone());
+
+    loop {
+        match last_result {
+            Code::Success(s) => {
+                if input.is_none() {
+                    *state = s;
+                    break true;
+                }
+                last_result = execute_operation(s, &mut || input.take().unwrap(), &mut |_| {
+                    panic!("Received unexpected output")
+                })
+            }
+            Code::Stop(s) => {
+                *state = s;
+                break false;
+            }
+        }
+    }
+}
+
 fn run_configuration(mem: Vec<isize>, settings: &[isize]) -> isize {
     let mut signal = 0;
     for &setting in settings.iter() {
@@ -141,7 +188,7 @@ fn main() {
     {
         let _timer = PerfTimer::new("Part 1");
         let mut part_1 = isize::MIN;
-        for config in (0..5).permutations(5) {
+        for config in (0..=4).permutations(5) {
             let result = run_configuration(input.clone(), &config);
             if result >= part_1 {
                 part_1 = result;
@@ -149,5 +196,33 @@ fn main() {
         }
 
         println!("Max signal: {part_1}");
+    }
+
+    {
+        let _timer = PerfTimer::new("Part 2");
+        let mut part_2 = 0;
+        for config in (5..=9).permutations(5) {
+            let mut current = 0;
+            let mut state: Vec<State> = (0..=4)
+                .map(|_| State {
+                    mem: input.clone(),
+                    pos: 0,
+                })
+                .collect();
+            for (k, v) in config.iter().copied().enumerate() {
+                assert!(run_one_in(&mut state[k], v))
+            }
+            'a: loop {
+                for s in state.iter_mut() {
+                    if let Some(c) = run_one_in_one_out(s, current) {
+                        current = c;
+                    } else {
+                        break 'a;
+                    }
+                }
+            }
+            part_2 = part_2.max(current);
+        }
+        println!("Part 2: {part_2}");
     }
 }
