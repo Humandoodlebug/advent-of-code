18a7b79a4f4bdd2f11df35b8a171575a55308425
Samuel Collins <samuel.collins@live.co.uk>
2022-10-15 14:52:39 +0100

    2019: day 12 - part 2

--- 2019/src/bin/day_12.rs
+++ 2019/src/bin/day_12.rs
@@ -3,7 +3,7 @@ use util::PerfTimer;
 
 type Point = (isize, isize, isize);
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 struct Moon {
     position: Point,
     velocity: Point,
@@ -27,40 +27,63 @@ fn input() -> Vec<Moon> {
     moons
 }
 
+fn do_step(moons: &mut [Moon]) {
+    for i in 0..moons.len() {
+        let mut moon = moons[i];
+        for m in &*moons {
+            match moon.position.0.cmp(&m.position.0) {
+                std::cmp::Ordering::Less => moon.velocity.0 += 1,
+                std::cmp::Ordering::Equal => (),
+                std::cmp::Ordering::Greater => moon.velocity.0 -= 1,
+            }
+            match moon.position.1.cmp(&m.position.1) {
+                std::cmp::Ordering::Less => moon.velocity.1 += 1,
+                std::cmp::Ordering::Equal => (),
+                std::cmp::Ordering::Greater => moon.velocity.1 -= 1,
+            }
+            match moon.position.2.cmp(&m.position.2) {
+                std::cmp::Ordering::Less => moon.velocity.2 += 1,
+                std::cmp::Ordering::Equal => (),
+                std::cmp::Ordering::Greater => moon.velocity.2 -= 1,
+            }
+        }
+        moons[i] = moon;
+    }
+
+    for moon in moons {
+        moon.position.0 += moon.velocity.0;
+        moon.position.1 += moon.velocity.1;
+        moon.position.2 += moon.velocity.2;
+    }
+}
+
+fn hcf(m: u128, n: u128) -> u128 {
+    assert!(m > 0);
+    assert!(n > 0);
+    let mut hcf = None;
+    for i in (1..=(m.min(n))).rev() {
+        if m % i == 0 && n % i == 0 {
+            hcf = Some(i);
+            break;
+        }
+    }
+    hcf.unwrap()
+}
+
+fn lcm(m: u128, n: u128) -> u128 {
+    assert!(m > 0);
+    assert!(n > 0);
+    (m * n) / hcf(m, n)
+}
+
 fn main() {
-    let moons = input();
+    let input = input();
 
     {
         let _timer = PerfTimer::new("Part 1");
-        let mut moons = moons.clone();
-        for _ in 0..1000 {
-            for i in 0..moons.len() {
-                let mut moon = moons[i];
-                for m in &moons {
-                    match moon.position.0.cmp(&m.position.0) {
-                        std::cmp::Ordering::Less => moon.velocity.0 += 1,
-                        std::cmp::Ordering::Equal => (),
-                        std::cmp::Ordering::Greater => moon.velocity.0 -= 1,
-                    }
-                    match moon.position.1.cmp(&m.position.1) {
-                        std::cmp::Ordering::Less => moon.velocity.1 += 1,
-                        std::cmp::Ordering::Equal => (),
-                        std::cmp::Ordering::Greater => moon.velocity.1 -= 1,
-                    }
-                    match moon.position.2.cmp(&m.position.2) {
-                        std::cmp::Ordering::Less => moon.velocity.2 += 1,
-                        std::cmp::Ordering::Equal => (),
-                        std::cmp::Ordering::Greater => moon.velocity.2 -= 1,
-                    }
-                }
-                moons[i] = moon;
-            }
-
-            for moon in &mut moons {
-                moon.position.0 += moon.velocity.0;
-                moon.position.1 += moon.velocity.1;
-                moon.position.2 += moon.velocity.2;
-            }
+        let mut moons = input.clone();
+        for _step in 0..1000 {
+            do_step(&mut moons);
         }
         let part_1: isize = moons
             .into_iter()
@@ -74,5 +97,39 @@ fn main() {
             )
             .sum();
         println!("Part 1: {part_1}");
+    }
+
+    {
+        let _timer = PerfTimer::new("Part 2");
+        let mut moons = input.clone();
+        let mut steps = (None, None, None);
+        let mut step = 0;
+        while steps.0.is_none() || steps.1.is_none() || steps.2.is_none() {
+            do_step(&mut moons);
+            step += 1;
+            if steps.0.is_none()
+                && moons.iter().enumerate().all(|(i, m)| {
+                    m.position.0 == input[i].position.0 && m.velocity.0 == input[i].velocity.0
+                })
+            {
+                steps.0 = Some(step);
+            }
+            if steps.1.is_none()
+                && moons.iter().enumerate().all(|(i, m)| {
+                    m.position.1 == input[i].position.1 && m.velocity.1 == input[i].velocity.1
+                })
+            {
+                steps.1 = Some(step);
+            }
+            if steps.2.is_none()
+                && moons.iter().enumerate().all(|(i, m)| {
+                    m.position.2 == input[i].position.2 && m.velocity.2 == input[i].velocity.2
+                })
+            {
+                steps.2 = Some(step);
+            }
+        }
+        let part_2 = lcm(lcm(steps.0.unwrap(), steps.1.unwrap()), steps.2.unwrap());
+        println!("Part 2: {part_2}");
     }
 }
