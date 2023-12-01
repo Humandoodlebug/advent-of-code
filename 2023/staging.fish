def32c3ff77669d42833eb79170667560b3c9cdd
Samuel Collins <samuel.collins@live.co.uk>
2023-12-01 19:19:20 +0000

    2023: staging script

---
+++ 2023/staging.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+argparse 's-safe' 'd-dirty' 'r-no-reset'  -- $argv
+
+set src (readlink -f (status dirname))
+
+if ! set -q STAGING
+    posix-source $src/.env
+end
+
+if ! set -q STAGING
+    echo '$STAGING is not set.'
+    exit 1
+end
+
+mkdir -p $STAGING
+cd $STAGING
+and set -q _flag_no_reset || git reset --hard
+and git pull --rebase
+and cp -r $src/* $src/.gitignore $STAGING/
+or exit
+
+not set -q _flag_dirty || exit 0
+git add --all
+and git commit
+or exit
+not set -q _flag_safe || exit 0
+git push || exit
